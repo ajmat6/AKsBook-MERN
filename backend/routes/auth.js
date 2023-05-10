7bea558967e6a209ecefc63dfcfb9e757421385f
ajmat6 <114720514+ajmat6@users.noreply.github.com>
2023-05-10 21:10:39 +0530

    Created thunderclient collections + setting up try catch + async await + already user exits logic

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,7 +8,7 @@ router.post('/',[ //creating an array of the validation
     body('username', 'Enter a valid Username').notEmpty(), //username should not be empty and the second '' in the body is the error message that you can give if username is empty.
     body('email', 'Enter a valid Email Address').isEmail(), // validation for the email
     body('password', 'Minimum length of the Password should be 8 characters').isLength({min: 8}), // password length should be minimum 8 characters
-], (req,res) => {
+], async (req,res) => {
     // res.json(obj); //sends json response
 
     // validation result khalli nahi he matlab error he therefore sending error to the user
@@ -17,32 +17,34 @@ router.post('/',[ //creating an array of the validation
        return res.status(400).json({ errors: result.array() });
     }
 
-    // check if the user with that particular username and email already exists:
-    let alreadyUsername = User.findOne({username: req.body.username});
-    let alreadyEmail = User.findOne({email: req.body.email});
-
-    if(alreadyUsername)
-    {
-        return res.status(400).json({error: "Username already exists"});
+    //Putting entire new user creation into try catch statements so that any error occurs other than valid user creation than those error can be known to us.
+    try {      
+        // check if the user with that particular username and email already exists:
+        let alreadyUsername = await User.findOne({username: req.body.username});
+        let alreadyEmail = await User.findOne({email: req.body.email});
+        
+        if(alreadyUsername)
+        {
+            return res.status(400).json({error: "Username already exists"});
+        }
+        
+        if(alreadyEmail)
+        {
+            return res.status(400).json({error: "This Email is already registered, Please enter a valid Email"});
+        }
+        
+        //Below is used to save the data in the DB and it is same as below two commented lines
+        user = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        })
+        
+        res.json(user);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("some error ocurred");
     }
-
-    if(alreadyEmail)
-    {
-        return res.status(400).json({error: "This Email is already registered, Please enter a valid Email"});
-    }
-
-    //Below is used to save the data in the DB and it is same as below two lines
-    User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    }).then((value) => {
-        res.json(value) // printing the recieved details in the console
-    }).catch((error) => {
-        console.log(error); // printing the error in the console
-        res.json({error: "Please enter valid details"}); //sending error response to the client
-    });
-
 
     // const user = User(req.body); // request me jo data aa raha he sending it to the schema of User.js
     // user.save(); //and then saving it and it will create react name db and users name collection in the db
