a9797f721c2e6414ab4905029170341e7b2dd731
ajmat6 <114720514+ajmat6@users.noreply.github.com>
2023-05-13 18:40:08 +0530

    React Router Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import{
+  BrowserRouter,
+  Routes,
+  Route
+}from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is AKsBook</h1>
-    </div>
+    <>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
